2c0c6f61123396d2de3813d6dbf91731e236dd80
ibanFR <ibanFR@users.noreply.github.com>
2026-02-16 19:29:17 +0100

    test: add combination tests for doUpdateQuality method

--- Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ Java-Approvals/src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -1,6 +1,8 @@
 package com.gildedrose;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.core.Options;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,23 @@ public class GildedRoseApprovalTest {
         app.updateQuality();
 
         Approvals.verifyAll("Items", items);
+    }
+
+    @Test
+    public void updateQualityOneDay() {
+
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
+            new String[]{"foo"},
+            new Integer[]{0},
+            new Integer[]{0},
+            new Options().withScrubber(String::trim));
+    }
+
+    public String doUpdateQuality(String foo, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(foo, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
     }
 
     @Test
